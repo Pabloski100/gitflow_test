76e68137ee16dbf87b4f8286b750dedfdf85752f
Pabloski100 <pablo.easton@hotmail.com>
2022-08-14 12:25:57 -0400

    funcion: top_retweeted

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 
+import json
 from unicodedata import name
 
+import json
 
 def top_retweeted():
-    pass
+    
+    tweet_list = []
+    max_tweet = []
+
+    with open("./farmers-protest-tweets-2021-03-5.json") as file:
+
+        for line in file:
+            data = json.loads(line)
+            tweet_list.append(data)
+
+    for i in range(10):
+        tweet_i = max(tweet_list, key=lambda x: int(x["retweetCount"]))
+        tweet_list.remove(tweet_i)
+        max_tweet.append(tweet_i)
+
+    return max_tweet
 
 def top_users():
     pass
