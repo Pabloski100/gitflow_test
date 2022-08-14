f44be24db733d72f7787d34821da6d9b1e64b797
Pabloski100 <pablo.easton@hotmail.com>
2022-08-14 11:51:53 -0400

    template

---
+++ test.py
@@ -0,0 +1,19 @@
+
+from unicodedata import name
+
+
+def top_retweeted():
+    pass
+
+def top_users():
+    pass
+
+def top_days():
+    pass
+
+def top_hashtags():
+    pass
+
+if __name__ == "__main__":
+    print("Main")
+    # Llamar funciones aqui
